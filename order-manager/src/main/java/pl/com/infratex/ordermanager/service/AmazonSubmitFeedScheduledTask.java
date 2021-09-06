c9afb7cf25b33d73079ff5c450d7f59a4e3b6d97
Pawel <pawelskibol@interia.pl>
2021-09-06 17:51:33 +0200

    unlock shipping confirmation module

--- order-manager/src/main/java/pl/com/infratex/ordermanager/service/AmazonSubmitFeedScheduledTask.java
+++ order-manager/src/main/java/pl/com/infratex/ordermanager/service/AmazonSubmitFeedScheduledTask.java
@@ -1,5 +1,6 @@
 package pl.com.infratex.ordermanager.service;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.logging.Logger;
@@ -15,10 +16,10 @@ public class AmazonSubmitFeedScheduledTask {
         this.shipmentConfirmationManagerService = shipmentConfirmationManagerService;
     }
 
-//    @Scheduled(fixedRate = 50000L)
+    @Scheduled(fixedRate = 500000L)
     public void task() {
-//        LOGGER.info("executing scheduled task");
-//        shipmentConfirmationManagerService.confirmShipment();
+        LOGGER.info("executing scheduled task");
+        shipmentConfirmationManagerService.confirmShipment();
     }
 
 }
