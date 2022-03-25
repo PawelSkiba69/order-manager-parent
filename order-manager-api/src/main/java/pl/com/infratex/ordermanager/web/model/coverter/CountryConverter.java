500d3d730e3d7f5d75ff6cfa1c7b007aca42aaa9
Pawel <pawelskibol@interia.pl>
2022-03-25 17:39:20 +0100

    add default value in CountryConverter

--- order-manager-api/src/main/java/pl/com/infratex/ordermanager/web/model/coverter/CountryConverter.java
+++ order-manager-api/src/main/java/pl/com/infratex/ordermanager/web/model/coverter/CountryConverter.java
@@ -7,10 +7,14 @@ import java.util.logging.Logger;
 public class CountryConverter {
 
     private static final Logger LOGGER = Logger.getLogger(CountryConverter.class.getName());
+
     public static CountryInfo countryConvert(String shortCut) {
 //        LOGGER.info("countryConvert("+shortCut+")");
-        CountryInfo countryInfo = CountryInfo.valueOf(shortCut);
-//        LOGGER.info("countryInfo: "+countryInfo);
-        return countryInfo;
+        try {
+            return CountryInfo.valueOf(shortCut);
+            //        LOGGER.info("countryInfo: "+countryInfo);
+        } catch (IllegalArgumentException e) {
+            return CountryInfo.UNKNOWN;
+        }
     }
 }
